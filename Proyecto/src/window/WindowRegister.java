36a4ae38727553154834f3705c30b5d46dbfa4c2
Asier "zJson" Garcia <asier.g.c@opendeusto.es>
2024-10-05 00:46:25 +0200

    EDIT - ./src/window/WindowLogin.java && WindowRegister.java structure

--- Proyecto/src/window/WindowRegister.java
+++ Proyecto/src/window/WindowRegister.java
@@ -2,10 +2,10 @@ package window;
 import javax.swing.JFrame;
 
 public class WindowRegister extends JFrame{
-		private static final long serialVersionUID = 1L;
-		public WindowRegister() {
-			setBounds(200,200,600,400);
+	private static final long serialVersionUID = 1L;
+	public WindowRegister() {
+		setBounds(200,200,600,400);
 			
-			setVisible(true);
-		}
+		setVisible(true);
 	}
+}
