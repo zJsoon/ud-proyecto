695964d1dcd44301c0d14d4e82110b0b4158393a
Asier "zJson" Garcia <asier.g.c@opendeusto.es>
2024-10-05 00:44:58 +0200

    EDIT - ./src/components/Users.java Editado para la nueva estructura.

--- Proyecto/src/components/Users.java
+++ Proyecto/src/components/Users.java
@@ -3,6 +3,8 @@ package components;
 public class Users {
 	private String username; // username of the user
 	private String pass; // password of the user
+	private String pass_confirm; // password confirm of the user
+	private String email; // email of the user
 	
 	/** Default constructor
 	 * 
@@ -14,11 +16,15 @@ public class Users {
 	/**	Create new user
 	 * @param username	user name of the user
 	 * @param pass	password of the user
+	 * @param pass_confirm	password confirm of the user
+	 * @param email	email of the user
 	 */
-	public Users(String username, String pass) {
+	public Users(String username, String pass, String pass_confirm, String email) {
 		super();
 		this.username = username;
 		this.pass = pass;
+		this.pass_confirm = pass_confirm;
+		this.email = email;
 	}
 	/** Get the user name 
 	 * @return String of the username gora 
@@ -48,10 +54,37 @@ public class Users {
 		this.pass = pass;
 	}
 	
+	/** Get the password confirm
+	 * @return	String of the password
+	 */
+	public String getPass_confirm() {
+		return pass_confirm;
+	}
+	
+	/** Enter the password confirm
+	 * @param username	String of the password confirm
+	 */
+	public void setPass_confirm(String pass_confirm) {
+		this.pass_confirm = pass_confirm;
+	}
+	
+	/** Get the email
+	 * @return	String of the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/** Enter the email
+	 * @param username	String of the email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	@Override
 	public String toString() {
 		return username+"\t"+pass;
 	}
 	
-	// comprobacion de conexion
 }
