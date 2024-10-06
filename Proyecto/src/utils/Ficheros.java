5b32d906f5bc9837b6c3b0401c35ad41c30d2947
Asier "zJson" Garcia <asier.g.c@opendeusto.es>
2024-10-06 15:44:09 +0200

    ADD - ./src/utils/Ficheros.java implementacion de metodos de ficheros.

---
+++ Proyecto/src/utils/Ficheros.java
@@ -0,0 +1,15 @@
+package utils;
+
+
+public class Ficheros {
+	public void cargarFicherosUsers(String fich) {
+		
+	}
+	public void leerFicherosUsers(String fich) {
+		
+	}
+	
+	public void editarFicheroUsers(String fich, String [] sp) {
+
+	}
+}
