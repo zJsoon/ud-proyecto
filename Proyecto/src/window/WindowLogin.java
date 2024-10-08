6cc6378e30b492517072e7c152cb485e44106e31
Asier "zJson" García <asier.g.c@opendeusto.es>
2024-10-08 16:08:22 +0200

    EDIT - Users.java and WindowLogin.java

--- Proyecto/src/window/WindowLogin.java
+++ Proyecto/src/window/WindowLogin.java
@@ -3,11 +3,15 @@ package window;
 import java.awt.*;
 import javax.swing.*;
 
+import window.logged.WindowLogged;
+
 public class WindowLogin extends JFrame{
 	private static final long serialVersionUID = 1L;
 	@SuppressWarnings("unused")
 	private JFrame wCurrent, wPrevious;
-	
+	private JTextField userText;
+	private JPasswordField passwordText;
+	@SuppressWarnings("deprecation")
 	public WindowLogin(JFrame wPrevious) {
 		super();
 		
@@ -16,33 +20,28 @@ public class WindowLogin extends JFrame{
 		
 		setBounds(200,200,600,400);
 		
-		
 		setTitle("UD Students - Main");
 		ImageIcon imagen = new ImageIcon("./img/logo-ud.png");
 		setIconImage(imagen.getImage());
-		
 		
 		// Creacion de JLabel y JText 
 		JLabel userLabel = new JLabel("Username");
 	    userLabel.setFont(new Font("Calibri", Font.CENTER_BASELINE, 30));
 	    userLabel.setHorizontalAlignment(SwingConstants.CENTER);
 	    
-	    JTextField userText = new JTextField();
+	    userText = new JTextField();
 	    
 	    JLabel passwordLabel = new JLabel("Password");
 	    passwordLabel.setFont(new Font("Calibri", Font.CENTER_BASELINE, 30));
 	    passwordLabel.setHorizontalAlignment(SwingConstants.CENTER);
 	    
-	    JTextField passwordText = new JTextField();
-	    
+	    passwordText = new JPasswordField();
 	    
 	    // Creacion de Botones
 	    JButton loginBotton = new JButton("Login");
 	    loginBotton.setFont(new Font("Calibri", Font.CENTER_BASELINE, 30));
 	    JButton exitBotton = new JButton("Exit");
 	    exitBotton.setFont(new Font("Calibri", Font.CENTER_BASELINE, 30));
-	    JButton registerBotton = new JButton("Register");
-	    registerBotton.setFont(new Font("Calibri", Font.CENTER_BASELINE, 30));
 		
 	    // Creacion de paneles
 	    JPanel panel = new JPanel();
@@ -60,7 +59,6 @@ public class WindowLogin extends JFrame{
 		
 	    panelSouth.add(loginBotton);
 	    panelSouth.add(exitBotton);
-	    panelSouth.add(registerBotton);
 	    
 	    panel.add(panelNorth, BorderLayout.NORTH);
 	    panel.add(panelSouth, BorderLayout.SOUTH);
@@ -75,8 +73,50 @@ public class WindowLogin extends JFrame{
 	    add(panel,BorderLayout.CENTER);
 		add(panelSouth, BorderLayout.SOUTH);
 	    
+		exitBotton.addActionListener(e -> {
+			wCurrent.dispose();
+			wPrevious.setVisible(true);
+		});
+		
+		loginBotton.addActionListener(e -> {
+			// Lógica del BOTÓN btnLogin
+			if(userText.getText().isBlank()) {
+				System.out.println("Rellene el apartado del usuario.");
+				JOptionPane.showMessageDialog(null, "Rellene el apartado del usuario.");
+			} else if(passwordText.getText().isBlank()) {
+				System.out.println("Rellene el apartado del password.");					
+				JOptionPane.showMessageDialog(null, "Rellene el apartado del password.");
+			} else if (userText.getText().isBlank() && passwordText.getText().isBlank()) {
+				System.out.println("Rellene los apartados usuario y password.");					
+				JOptionPane.showMessageDialog(null, "Rellene los apartados usuario y password.");					
+			} else if(userText.getText().equals("123") && passwordText.getText().equals("123")) {
+				System.out.println("Has iniciado sesión correctamente.");
+				JOptionPane.showMessageDialog(null, "Has iniciado sesión correctamente.");
+				vaciarCampos(1);
+				wCurrent.dispose(); // Cierro ventana actual
+				new WindowLogged(wCurrent); // Abrimos la ventana2 indicando que su ventana anterior es (this).
+			} else if(!userText.getText().equals("123")) {
+				System.out.println("Usuario incorrecto.");					
+				JOptionPane.showMessageDialog(null, "Usuario incorrecto.");
+				vaciarCampos(2);
+			} else if(!passwordText.getText().equals("123")) {
+				System.out.println("Contraseña incorrecta.");
+				JOptionPane.showMessageDialog(null, "Contraseña incorrecta.");
+				vaciarCampos(3);
+		}
+		});
 	    
 	    setVisible(true);
 	
+	}
+	public void vaciarCampos (int i) {
+		if (i == 1) {
+			userText.setText("");
+			passwordText.setText("");
+		} else if ( i == 2 ) {
+			userText.setText("");
+		} else if (i == 3 ) {
+			passwordText.setText("");
+		}
 	}
 }
