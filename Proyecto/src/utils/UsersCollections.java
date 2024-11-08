795bd31580a002ca7270ff2eb4392faf5b115caf
Asier "zJson" García <asier.g.c@opendeusto.es>
2024-11-08 16:32:24 +0100

    ADD - methods verificationUser & userIsAdmin

--- Proyecto/src/utils/UsersCollections.java
+++ Proyecto/src/utils/UsersCollections.java
@@ -1,11 +1,82 @@
 package utils;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import components.Users;
+import db.ConnectionDB;
+
 public class UsersCollections {
-	public static void verificationUser() {
+	private boolean verified = false;
+	private boolean adminVerified = false;
+	
+	public boolean verificationUser(String u_id, String passw) {
+		ConnectionDB.ConnectJDBC();
 		
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:resources/db/users.db");
+			System.out.println("He accedido correctamente.");
+			
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM usuario");		
+					
+			List<Users> usuarios = new ArrayList<Users>();
+			
+			while(rs.next()) {
+				String user = rs.getString("user");
+				String pass = rs.getString("password");
+				
+				usuarios.add(new Users(user, pass));
+			}
+			
+			for (Users u : usuarios) {
+				if(u.getUsername().equals(u_id) && u.getPass().equals(passw)) {
+					verified = true;
+					break;
+				}
+			}
+			
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return verified;
 	}
 	
-	public static void userIsAdmin() {
+	public boolean userIsAdmin(String u_id) {
+		ConnectionDB.ConnectJDBC();
 		
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:resources/db/users.db");
+			System.out.println("He accedido correctamente.");
+			
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM usuario");		
+					
+			List<Users> usuarios = new ArrayList<Users>();
+			
+			while(rs.next()) {
+				String user = rs.getString("user");
+				String pass = rs.getString("password");
+				boolean admin = rs.getBoolean("admin");
+				
+				usuarios.add(new Users(user, pass, admin));
+			}
+			
+			for (Users u : usuarios) {
+				if(u.getUsername().equals(u_id) && u.getAdmin() == true) {
+					adminVerified = true;
+					break;
+				}
+			}
+			
+			stmt.close();
+			conn.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return adminVerified;
 	}
 }
