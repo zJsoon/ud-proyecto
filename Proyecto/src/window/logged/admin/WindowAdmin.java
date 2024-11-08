fd1c2b0aa4526c4953dd3c98171b0894b13a5f3e
Asier "zJson" García <asier.g.c@opendeusto.es>
2024-11-08 16:05:04 +0100

    ADD - WindowAdmin options for admin.

---
+++ Proyecto/src/window/logged/admin/WindowAdmin.java
@@ -0,0 +1,13 @@
+package window.logged.admin;
+
+import javax.swing.*;
+
+public class WindowAdmin extends JFrame{
+	private static final long serialVersionUID = 1L;
+
+	public WindowAdmin() {
+		super();
+		
+		setVisible(true);
+	}
+}
