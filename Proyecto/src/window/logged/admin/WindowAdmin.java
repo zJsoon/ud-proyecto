dbae9f552ded4965407c0adbdaaff319378d1ee6
Asier "zJson" García <asier.g.c@opendeusto.es>
2024-11-11 18:13:31 +0100

    EDIT - Añadido parametros wPrevious y Users

--- Proyecto/src/window/logged/admin/WindowAdmin.java
+++ Proyecto/src/window/logged/admin/WindowAdmin.java
@@ -2,11 +2,18 @@ package window.logged.admin;
 
 import javax.swing.*;
 
+import components.Users;
+
 public class WindowAdmin extends JFrame{
 	private static final long serialVersionUID = 1L;
+	@SuppressWarnings("unused")
+	private JFrame wCurrent, wPrevious;
 
-	public WindowAdmin() {
+	public WindowAdmin(JFrame wPrevious, Users u) {
 		super();
+		
+		this.wPrevious = wPrevious;
+		wCurrent = this;
 		
 		setVisible(true);
 	}
