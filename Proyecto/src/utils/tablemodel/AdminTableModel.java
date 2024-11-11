22dc27074f5b00e91faa2e487336baecc1778f07
Asier "zJson" García <asier.g.c@opendeusto.es>
2024-11-11 18:25:35 +0100

    ADD - Table Models for the admin menu

---
+++ Proyecto/src/utils/tablemodel/AdminTableModel.java
@@ -0,0 +1,8 @@
+package utils.tablemodel;
+
+import javax.swing.table.DefaultTableModel;
+
+public class AdminTableModel extends DefaultTableModel{
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ Proyecto/src/utils/tablemodel/SeriesUsersTableModel.java
@@ -0,0 +1,8 @@
+package utils.tablemodel;
+
+import javax.swing.table.DefaultTableModel;
+
+public class SeriesUsersTableModel extends DefaultTableModel{
+	private static final long serialVersionUID = 1L;
+
+}
