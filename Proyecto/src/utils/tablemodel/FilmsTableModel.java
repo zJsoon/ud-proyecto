cfcf8f91eda36fa3ced270c783233bd532f0c29e
Aimar Ariznabarreta <aimar.ariznabarreta@opendeusto.es>
2024-11-21 19:18:41 +0100

    ADD - ./src/tablemodel/FilmsTableMode/. - Finalizar la clase con los
    construtores

--- Proyecto/src/utils/tablemodel/FilmsTableModel.java
+++ Proyecto/src/utils/tablemodel/FilmsTableModel.java
@@ -14,5 +14,41 @@ public class FilmsTableModel extends DefaultTableModel{
 	private List<String> lTitles = Arrays.asList("Title","Year","Rating","Duration");
 	
 	
-
-
+	public FilmsTableModel(List<Films> lf) {
+        lFilms = lf;
+    }
+	
+	@Override
+	public int getRowCount() {
+		if (lFilms == null)
+			return 0;
+		return lFilms.size();
+	}
+	
+	@Override
+	public int getColumnCount() {
+		return lTitles.size();
+	}
+	
+	@Override
+	public String getColumnName(int column) {
+		return lTitles.get(column);
+	}
+	
+	@Override
+	public Object getValueAt(int row, int column) {
+		Films f = lFilms.get(row);
+		switch (column) {
+		case 0:
+			return f.getTitle();
+		case 1:
+			return f.getYr();
+		case 2:
+			return f.getRating();
+		case 3:
+			return f.getDuration();
+		default:
+			return null;
+		}
+	}
+}
