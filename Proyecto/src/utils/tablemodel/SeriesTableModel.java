95122008424972a84b03822d6cc177b0577c1180
Aimar Ariznabarreta <aimar.ariznabarreta@opendeusto.es>
2024-11-21 19:30:00 +0100

    ADD - ./src/tablemodel/SeriesTableModel/. - Añadir las listas a la clase

--- Proyecto/src/utils/tablemodel/SeriesTableModel.java
+++ Proyecto/src/utils/tablemodel/SeriesTableModel.java
@@ -1,8 +1,16 @@
 package utils.tablemodel;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.table.DefaultTableModel;
+
+import components.Series;
 
 public class SeriesTableModel extends DefaultTableModel{
 	private static final long serialVersionUID = 1L;
+	
+	private List<Series> lSeries;
+	private List<String> lTitulos = Arrays.asList("NOMBRE","TEMPORADAS","CAPITULOS");
 
 }
