18397c5ec3faa3b910a109f55e599d3759c791b3
Aimar Ariznabarreta <aimar.ariznabarreta@opendeusto.es>
2024-11-21 19:09:42 +0100

    ADD - ./src/utils.tablemodel/FilmsTableMode/. - Añadir listas

--- Proyecto/src/utils/tablemodel/FilmsTableModel.java
+++ Proyecto/src/utils/tablemodel/FilmsTableModel.java
@@ -1,8 +1,18 @@
 package utils.tablemodel;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.table.DefaultTableModel;
+
+import components.Films;
 
 public class FilmsTableModel extends DefaultTableModel{
 	private static final long serialVersionUID = 1L;
+	
+	private List<Films> lFilms;
+	private List<String> lTitles = Arrays.asList("Title","Year","Rating","Duration");
+	
+	
 
-}
+
