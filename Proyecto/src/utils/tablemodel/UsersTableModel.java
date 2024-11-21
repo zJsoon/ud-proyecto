06618704cec1b08b3cc2c608f3aab35cba771453
Aimar Ariznabarreta <aimar.ariznabarreta@opendeusto.es>
2024-11-21 20:18:23 +0100

    ADD - ./src/tablemode/UsersTableMode/. - Añadir las listas

--- Proyecto/src/utils/tablemodel/UsersTableModel.java
+++ Proyecto/src/utils/tablemodel/UsersTableModel.java
@@ -1,8 +1,22 @@
 package utils.tablemodel;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.table.DefaultTableModel;
+
+import components.Users;
 
 public class UsersTableModel extends DefaultTableModel{
 	private static final long serialVersionUID = 1L;
+	
+	private List<Users> lUsers;
+	private List<String> lTitles = Arrays.asList("Name","Surname","Age","Email");
+	
+	
+	
+	
+	
+	
 
 }
