bee43e369535fcb593f42cbd54ca9d64f1e732da
Aimar Ariznabarreta <aimar.ariznabarreta@opendeusto.es>
2024-11-21 19:32:41 +0100

    ADD - ./src/tablemodel/SeriesTableModel/. - Añadir los contructores a la
    clase

--- Proyecto/src/utils/tablemodel/SeriesTableModel.java
+++ Proyecto/src/utils/tablemodel/SeriesTableModel.java
@@ -11,6 +11,42 @@ public class SeriesTableModel extends DefaultTableModel{
 	private static final long serialVersionUID = 1L;
 	
 	private List<Series> lSeries;
-	private List<String> lTitulos = Arrays.asList("NOMBRE","TEMPORADAS","CAPITULOS");
+	private List<String> lTitulos = Arrays.asList("NOMBRE","TEMPORADAS","DURACION MEDIA");
+	
+	public SeriesTableModel(List<Series> ls) {
+		lSeries = ls;
+	}
+	
+	@Override
+	public int getRowCount() {
+		if (lSeries == null)
+			return 0;
+		return lSeries.size();
+	}
+	
+	@Override
+	public int getColumnCount() {
+		return lTitulos.size();
+	}
+	
+	@Override
+	public String getColumnName(int column) {
+		return lTitulos.get(column);
+	}
+	
+	@Override
+	public Object getValueAt(int row, int column) {
+		Series s = lSeries.get(row);
+		switch (column) {
+		case 0:
+			return s.getTitle();
+		case 1:
+			return s.getSeasons();
+		case 2:
+			return s.getAverageDuration();
+		default:
+			return null;
+		}
+	}
 
 }
