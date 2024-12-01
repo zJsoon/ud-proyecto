4bad474f3a76892f85673bb46008f788eb736c40
Asier "zJson" Garcia <asier.g.c@opendeusto.es>
2024-12-01 19:15:51 +0100

    Add methods

--- Proyecto/src/utils/tablemodel/UsersTableModel.java
+++ Proyecto/src/utils/tablemodel/UsersTableModel.java
@@ -11,12 +11,43 @@ public class UsersTableModel extends DefaultTableModel{
 	private static final long serialVersionUID = 1L;
 	
 	private List<Users> lUsers;
-	private List<String> lTitles = Arrays.asList("Name","Surname","Age","Email");
+	private List<String> lTitles = Arrays.asList("Username", "Password", "Email", "Admin");
 	
+	public UsersTableModel(List<Users> lu) {
+		lUsers = lu;
+	}
 	
-	
-	
-	
-	
+	@Override
+	public int getRowCount() {
+		if(lUsers == null)
+			return 0;
+		return lUsers.size();
+	}
 
+	@Override
+	public int getColumnCount() {
+		return lTitles.size();
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return lTitles.get(column);
+	}
+
+	@Override
+	public boolean isCellEditable(int row, int column) {
+		return false;
+	}
+
+	@Override
+	public Object getValueAt(int row, int column) {
+		Users u = lUsers.get(row);
+		switch(column) {
+			case 0: return u.getUsername();
+			case 1: return u.getPass();
+			case 2: return u.getEmail();
+			case 3: return u.getAdmin();
+			default : return null;
+		}
+	}
 }
