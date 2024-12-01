62c11c82b6f02ab731b9ee0e271dfe2aceecc8d8
Asier "zJson" Garcia <asier.g.c@opendeusto.es>
2024-12-01 19:16:23 +0100

    ADD - Visualizar en una tabla y usando Jtree los usuarios vista de admin

--- Proyecto/src/window/logged/admin/WindowAdmin.java
+++ Proyecto/src/window/logged/admin/WindowAdmin.java
@@ -1,20 +1,74 @@
 package window.logged.admin;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreePath;
 
 import components.Users;
+import utils.collections.ConnectionDB;
+import utils.tablemodel.UsersTableModel;
 
 public class WindowAdmin extends JFrame{
 	private static final long serialVersionUID = 1L;
 	@SuppressWarnings("unused")
 	private JFrame wCurrent, wPrevious;
-
+	private JPanel pWest;
+	private DefaultTreeModel treeModel;
+	private JTree tree;
+	private List<String> lAttributes = Arrays.asList("user", "admins", "series", "films", "series_user", "films_users");
+	private JScrollPane scrollTree;
+	
+	private UsersTableModel userModel;
+	private JTable userTabla;
+	@SuppressWarnings("unused")
+	private JScrollPane userScroll;
+	
 	public WindowAdmin(JFrame wPrevious, Users u) {
 		super();
 		
 		this.wPrevious = wPrevious;
 		wCurrent = this;
 		
+		setBounds(300, 200, 600, 400);
+		pWest = new JPanel();
+		/*Creación del JTree*/
+		DefaultMutableTreeNode raiz = new DefaultMutableTreeNode("ADMIN PANEL");
+		treeModel = new DefaultTreeModel(raiz);
+		tree = new JTree(treeModel);
+		scrollTree = new JScrollPane(tree);
+		pWest.add(scrollTree);
+		
+		/* DB */
+		ConnectionDB db_u = new ConnectionDB();
+		db_u.connectJDBC("resources/db/db_project.sqbpro");
+		List<Users> lu = db_u.obtainUsers();
+		
+		userModel = new UsersTableModel(null);
+		userTabla = new JTable(userModel);
+		userScroll = new JScrollPane(userTabla);
+	    
+		tree.addTreeSelectionListener( e -> {
+			TreePath tp = e.getPath();
+			String p = tp.getLastPathComponent().toString();
+			if(p.equals("user")) {
+				userModel = new UsersTableModel(lu);
+				userTabla.setModel(userModel);
+			}
+		});
+		db_u.disconnectJDBC();
+		
 		setVisible(true);
+	}
+	
+	public void loadTree() {
+		for(int pos = 0;lAttributes.size()<pos;pos++) {
+			DefaultMutableTreeNode newMTN = new DefaultMutableTreeNode(lAttributes.get(pos));
+			treeModel.insertNodeInto(newMTN, (MutableTreeNode) treeModel.getRoot(), pos);
+		}
 	}
 }
