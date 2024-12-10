59ca07fc68a0a657e9e149afa5a9a7d7852b89ab
Asier "zJson" García <asier.g.c@opendeusto.es>
2024-12-10 18:39:50 +0100

    EDIT - Normal MAIN

--- Proyecto/src/main/Main.java
+++ Proyecto/src/main/Main.java
@@ -1,12 +1,9 @@
 package main;
 
-import components.Users;
 import window.*;
-import window.logged.admin.WindowAdmin;
 
 public class Main {
 	public static void main(String[] args) {
 		new WindowMain();
-		//new WindowAdmin(null, new Users());
 	}
 }
