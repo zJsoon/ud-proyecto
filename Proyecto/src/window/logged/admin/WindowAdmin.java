69c6d037381e0abde32cb966944b6c4b7342cabe
Asier "zJson" Garcia <asier.g.c@opendeusto.es>
2025-01-12 17:01:15 +0100

    FIX - WindowAdmin

--- Proyecto/src/window/logged/admin/WindowAdmin.java
+++ Proyecto/src/window/logged/admin/WindowAdmin.java
@@ -1,5 +1,6 @@
 package window.logged.admin;
 
+import java.awt.BorderLayout;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,7 +19,7 @@ public class WindowAdmin extends JFrame{
 	private static final long serialVersionUID = 1L;
 	@SuppressWarnings("unused")
 	private JFrame wCurrent, wPrevious;
-	private JPanel pWest;
+	private JPanel pWest,pCenter;
 	private DefaultTreeModel treeModel;
 	private JTree tree;
 	private List<String> lAttributes = Arrays.asList("user", "admins", "series", "films", "series_user", "films_users");
@@ -36,24 +37,27 @@ public class WindowAdmin extends JFrame{
 		this.wPrevious = wPrevious;
 		wCurrent = this;
 		
-		setBounds(200, 200, 600, 400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setTitle("US Students - Admin");
+		setTitle("UD Students - Admin");
+		setBounds(200, 200, 600, 400);
 		setResizable(false);
 		ImageIcon imagen = new ImageIcon("./img/logo-ud.png");
 		setIconImage(imagen.getImage());
 		
 		pWest = new JPanel();
+		pCenter = new JPanel();
 		/*Creación del JTree*/
 		DefaultMutableTreeNode raiz = new DefaultMutableTreeNode("ADMIN PANEL");
 		treeModel = new DefaultTreeModel(raiz);
 		tree = new JTree(treeModel);
 		scrollTree = new JScrollPane(tree);
+		loadTree();
 		pWest.add(scrollTree);
 		
 		/* DB */
 		DB db_u = new DB();
-		db_u.connectJDBC("resources/db/db_project.db");
+		db_u.connectJDBC("resources\\db\\db_proyecto.db");
+		//System.out.println(lAttributes.size());
 		List<Users> lu = db_u.obtainUsers();
 		List<Users> la = db_u.obtainUsersAdmin();
 		
@@ -65,12 +69,16 @@ public class WindowAdmin extends JFrame{
 		adminTabla = new JTable(adminModel);
 		adminScroll = new JScrollPane(adminTabla);
 		
+		getContentPane().add(pWest, BorderLayout.WEST);
+		getContentPane().add(pCenter, BorderLayout.CENTER);
+		
 		tree.addTreeSelectionListener( e -> {
 			TreePath tp = e.getPath();
 			String p = tp.getLastPathComponent().toString();
 			if(p.equals("user")) {
 				userModel = new UsersTableModel(lu);
 				userTabla.setModel(userModel);
+				getContentPane().add(userScroll, BorderLayout.CENTER);
 			} else if (p.equals("admins")) {
 				adminModel = new AdminTableModel(la);
 				adminTabla.setModel(adminModel);
@@ -94,7 +102,8 @@ public class WindowAdmin extends JFrame{
 	}
 	
 	public void loadTree() {
-		for(int pos = 0;lAttributes.size()<pos;pos++) {
+		for(int pos = 0;lAttributes.size()>pos;pos++) {
+			System.out.println(pos);
 			DefaultMutableTreeNode newMTN = new DefaultMutableTreeNode(lAttributes.get(pos));
 			treeModel.insertNodeInto(newMTN, (MutableTreeNode) treeModel.getRoot(), pos);
 		}
