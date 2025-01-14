304a1edcb445dbf7060876b523ca942ec840de63
Asier "zJson" Garcia <asier.g.c@opendeusto.es>
2025-01-14 22:01:44 +0100

    REMOVE

--- Proyecto/src/utils/tablemodel/FilmsUsersTableModel.java
+++
@@ -1,8 +0,0 @@
-package utils.tablemodel;
-
-import javax.swing.table.DefaultTableModel;
-
-public class FilmsUsersTableModel extends DefaultTableModel{
-	private static final long serialVersionUID = 1L;
-	
-}
